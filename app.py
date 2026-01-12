1c813430b7bd68ce51f7ac1a833bf8c5c4ba2c70
dacent53 <gneo0194@gmail.com>
2026-01-12 19:31:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -43,5 +43,5 @@ tiktok x-ladon, tiktok x-argus, tiktok x-gorgon, tiktok x-khronos, tiktok device
 22. create accounts
 
 ### contact me
-## Contact Telegram @alethino
-https://t.me/alethino
+## Contact Telegram @dacent53
+https://t.me/dacent53
